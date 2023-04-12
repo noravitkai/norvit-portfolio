8415000ebf13cabf6ef0f7c82d1b6494ce4858bf
noravitkai <norvit01@easv365.dk>
2023-04-12 13:20:57 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,6 @@ import HelloWorld from "./components/HelloWorld.vue";
 
 <template>
   <div>
-    <h1 class="text-4xl font-bold">Hello World</h1>
+    <h1 class="text-4xl font-medium">Hello World</h1>
   </div>
 </template>
